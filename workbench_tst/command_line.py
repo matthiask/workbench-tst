0d066804a225ea25dcef36cacd08f28b3b1e4505
Matthias Kestenholz <mk@feinheit.ch>
2020-04-02 11:27:54 +0200

    Validate the configuration a bit

--- workbench_tst/command_line.py
+++ workbench_tst/command_line.py
@@ -9,7 +9,14 @@ from urllib.request import urlopen
 
 def main():
     config = configparser.ConfigParser()
-    config.read(pathlib.Path("~/.workbench").expanduser())
+    try:
+        config.read_string(pathlib.Path("~/.workbench").expanduser().read_text())
+    except FileNotFoundError:
+        sys.stderr.write("Config file ~/.workbench is missing\n")
+        sys.exit(1)
+
+    user = config.get("workbench", "user")
+    url = config.get("workbench", "url")
 
     if len(sys.argv) < 2:
         sys.stderr.write(
@@ -17,7 +24,7 @@ def main():
         )
         sys.exit(1)
 
-    data = {"type": sys.argv[1], "user": config.get("workbench", "user")}
+    data = {"type": sys.argv[1], "user": user}
 
     if len(sys.argv) > 2 and re.match(r"[0-9]{1,2}:[0-9]{2}", sys.argv[2]):
         data["time"] = sys.argv[2]
@@ -26,7 +33,7 @@ def main():
         data["notes"] = " ".join(sys.argv[2:])
 
     try:
-        urlopen(config.get("workbench", "url"), urlencode(data).encode("utf-8"))
+        urlopen(url, urlencode(data).encode("utf-8"))
     except HTTPError:
         sys.stderr.write("FAILURE\n")
         sys.exit(1)
